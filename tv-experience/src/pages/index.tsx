a888d6e34388166e70e7aebde487e3405bb34bae
Hugo Bessa <eu@hugobessa.com.br>
2021-06-28 20:42:06 -0300

    Configure Stitches for component CSS

--- tv-experience/src/pages/index.tsx
+++ tv-experience/src/pages/index.tsx
@@ -1,4 +1,59 @@
 import Head from 'next/head';
+import {styled} from '@/theme.config';
+
+let Menubar = styled('nav', {});
+
+let MenubarList = styled('ul', {
+  background: 'hsla(222, 36%, 11%, 0.7)',
+  borderRadius: 100,
+  backdropFilter: 'blur(15px);',
+  display: 'grid',
+  gridAutoFlow: 'column',
+  gridAutoColumns: 'max-content',
+  gap: 20,
+  padding: 5,
+  width: 'max-content',
+
+  listStyleType: 'none',
+  margin: 0,
+});
+
+let MenubarItem = styled('li', {});
+
+let MenubarButton = styled('a', {
+  display: 'inline-block',
+  padding: '5px 30px',
+  background: 'hsla(219, 9%, 65%, 1)',
+  color: 'white',
+  borderRadius: 100,
+});
+
+function Header() {
+  return (
+    <header>
+      <div />
+      <Menubar>
+        <MenubarList>
+          <MenubarItem>
+            <MenubarButton href='#'>Most Visited</MenubarButton>
+          </MenubarItem>
+          <MenubarItem>
+            <MenubarButton href='#'>Biggest</MenubarButton>
+          </MenubarItem>
+          <MenubarItem>
+            <MenubarButton href='#'>By State</MenubarButton>
+          </MenubarItem>
+          <MenubarItem>
+            <MenubarButton href='#'>Search</MenubarButton>
+          </MenubarItem>
+          <MenubarItem>
+            <MenubarButton href='#'>Settings</MenubarButton>
+          </MenubarItem>
+        </MenubarList>
+      </Menubar>
+    </header>
+  );
+}
 
 export default function Home() {
   return (
@@ -10,6 +65,7 @@ export default function Home() {
           content='A TV-like experience built with Next.js highlighting US National Parks'
         />
       </Head>
+      <Header />
     </div>
   );
 }
