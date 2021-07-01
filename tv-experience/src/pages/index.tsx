5aa31f24259e9eb87d7f4fc7d958cfe8a77fe563
Hugo Bessa <eu@hugobessa.com.br>
2021-07-01 06:25:25 -0300

    Add wip of BackgroundImage

--- tv-experience/src/pages/index.tsx
+++ tv-experience/src/pages/index.tsx
@@ -1,5 +1,27 @@
 import Head from 'next/head';
 import Header from '@/components/Header';
+import {styled} from '@/theme.config';
+
+let BackgroundImageRoot = styled('div', {
+  position: 'fixed',
+  zIndex: '$min',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: '25%',
+  background: '$gray1',
+  backgroundSize: 'cover',
+});
+
+type BackgroundImageProps = {
+  backgroundImage: string;
+};
+
+function BackgroundImage({backgroundImage}: BackgroundImageProps) {
+  return (
+    <BackgroundImageRoot css={{backgroundImage: `url(${backgroundImage})`}} />
+  );
+}
 
 export default function Home() {
   return (
@@ -11,6 +33,7 @@ export default function Home() {
           content='A TV-like experience built with Next.js highlighting US National Parks'
         />
       </Head>
+      <BackgroundImage backgroundImage='/images/grand-canyon/gert-boers-qQC8tyG_JVA-unsplash.jpg' />
       <Header />
     </div>
   );
